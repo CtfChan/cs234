bb70d11aeb47c9f4a52cda9f3c0de35ab4c91b90
Christopher Chan <christopherchan@pop-os.localdomain>
2020-07-26 10:47:19 -0400

    adding a2

--- a1/vi_and_pi.py
+++ a1/vi_and_pi.py
@@ -164,8 +164,6 @@ def value_iteration(P, nS, nA, gamma=0.9, tol=1e-3):
 	"""
 
 	value_function = np.zeros(nS)
-	print(value_function)
-	print("====")
 	policy = np.zeros(nS, dtype=int)
 	############################
 	# YOUR IMPLEMENTATION HERE #
@@ -234,13 +232,13 @@ def render_single(env, policy, max_steps=100):
 if __name__ == "__main__":
 
 	# comment/uncomment these lines to switch between deterministic/stochastic environments
-	env = gym.make("Deterministic-4x4-FrozenLake-v0")
-	# env = gym.make("Stochastic-4x4-FrozenLake-v0")
+	# env = gym.make("Deterministic-4x4-FrozenLake-v0")
+	env = gym.make("Stochastic-4x4-FrozenLake-v0")
 
-	# print("\n" + "-"*25 + "\nBeginning Policy Iteration\n" + "-"*25)
+	print("\n" + "-"*25 + "\nBeginning Policy Iteration\n" + "-"*25)
 
-	# V_pi, p_pi = policy_iteration(env.P, env.nS, env.nA, gamma=0.9, tol=1e-3)
-	# render_single(env, p_pi, 100)
+	V_pi, p_pi = policy_iteration(env.P, env.nS, env.nA, gamma=0.9, tol=1e-3)
+	render_single(env, p_pi, 100)
 
 	print("\n" + "-"*25 + "\nBeginning Value Iteration\n" + "-"*25)
 
